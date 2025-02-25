b12830241b098cc945476c351033834b9c6864ac
R. Shadetos <shadetos@gmail.com>
2025-02-24 17:10:10 -0800

    Configure local dev option to review

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/shade-portfolio",
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
